ec2b4fd9c1f1ac3a5e5a8dfafd761f4abcbd12ba
Siddharudh1 <110817247+Siddharudh1@users.noreply.github.com>
2022-09-07 17:13:56 +0530

    Delete BankingSystem directory

--- BankingSystem/src/main/java/com/example/bankingsystem/AccountRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.bankingsystem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface AccountRepo extends JpaRepository<Account,Integer> {
-	
-	
-
-}
